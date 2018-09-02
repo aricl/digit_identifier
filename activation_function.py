888bcceccc840b72a8621c375ffb1ea000401f65
Adam John Ready <worldelephantseal@gmail.com>
2018-09-02 10:04:59 +0100

    Initial commit

---
+++ activation_function.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+def sigmoid():
+    """
+    Returns a lambda function that takes the form of the sigmoid function.
+    :return: float
+    """
+    return lambda z: 1 / (1 + np.exp(-z))
+
+
+def perceptron():
+    """
+    Returns a lambda function that takes the form of the perceptron or step
+    function.
+    :return: int
+    """
+    return lambda z: 1 if z > 0 else 0
+
+
+def relu():
+    """
+    Returns a lambda function that takes the form of the rectified linear
+    unit or ReLU function.
+    :return: float
+    """
+    return lambda z: z if z > 0 else 0
