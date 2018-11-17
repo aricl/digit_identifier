86c973a32cfa6d990caa3c0f1b65a885ffeadf67
Adam John Ready <worldelephantseal@gmail.com>
2018-11-17 17:27:17 +0000

    Refactored main.py to include more explicit variable names and decoupling initialisation of weights and biases

--- main.py
+++ main.py
@@ -6,32 +6,36 @@ if __name__ == '__main__':
     number_of_inputs = 10
     number_of_hidden_neurons = 12
 
-    neurons_parameters = [
-        [
-            np.random.uniform(0, 1, number_of_inputs),
-            np.random.uniform(-5, 0)
-        ]
+    hidden_layer_weights = [
+        np.random.uniform(0, 1, number_of_inputs)
         for i in range(0, number_of_hidden_neurons)
     ]
 
-    neurons = [
+    hidden_layer_biases = [
+        np.random.uniform(-5, 0)
+        for i in range(0, number_of_hidden_neurons)
+    ]
+
+    hidden_layer_neurons = [
         hidden_layer_neuron.HiddenLayerNeuron(
-            neuron_parameters[0],
-            neuron_parameters[1],
+            hidden_layer_weights[i],
+            hidden_layer_biases[i],
             activation_function.sigmoid()
         )
-        for neuron_parameters in neurons_parameters
+        for i in range(0, number_of_hidden_neurons)
     ]
 
-    neurons_output = [
+    hidden_layer_neurons_output = [
         neuron.output(np.random.uniform(0, 1, number_of_inputs))
-        for neuron in neurons
+        for neuron in hidden_layer_neurons
     ]
 
+    output_neuron_weights = np.random.uniform(0, 1, number_of_hidden_neurons)
+    output_neuron_bias = np.random.uniform(-5, 0)
     output_neuron = hidden_layer_neuron.HiddenLayerNeuron(
-        np.random.uniform(0, 1, number_of_hidden_neurons),
-        np.random.uniform(-5, 0),
+        output_neuron_weights,
+        output_neuron_bias,
         activation_function.sigmoid()
     )
 
-    print(output_neuron.output(neurons_output))
+    print(output_neuron.output(hidden_layer_neurons_output))
